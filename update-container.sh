4d727e56defaf63a409e07dd3ea2544924e7135b
Noans <jonjontan610@hotmail.com>
2020-09-06 18:02:07 +0800

    Created dedicated folder for docker deployment files

--- update-container.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker cp ./ crawler-api:/opt/scoutspyder/
-docker exec crawler-api rm -rf /opt/scoutspyder/{.git,.gitignore,Dockerfile,requirements.txt,update-container.sh,docker-compose.yml}
-docker restart crawler-api
