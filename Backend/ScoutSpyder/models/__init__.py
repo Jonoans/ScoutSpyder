641aeba4018b4076753cc6adba841479c066046b
Noans <jonjontan610@hotmail.com>
2020-09-11 23:16:37 +0800

    Added TLS option for database

--- Backend/ScoutSpyder/models/__init__.py
+++ Backend/ScoutSpyder/models/__init__.py
@@ -17,12 +17,28 @@ def db_conn_init():
     name = config['DATABASE']['Name']
     host = config['DATABASE']['Host']
     port = int(config['DATABASE']['Port'])
+    username = config['DATABASE'].get('username')
+    if username:
+        username = username.strip() if username.strip() else None
+    password = config['DATABASE'].get('password')
+    if password:
+        password = password.strip() if username and password.strip() else None
+    tls = config['DATABASE'].getboolean('TlsEnabled', fallback=False)
+    tls_ca_file = config['DATABASE'].get('TlsCaCertificate') if tls else None
+
+    args = {
+        'alias': 'global',
+        'name': name,
+        'host': host,
+        'port': port,
+        'username': username,
+        'password': password,
+        'tls': tls,
+        'tlsCAFile': tls_ca_file
+    }
 
     # Register connection
-    mongoengine.register_connection(
-        alias='global', name=name,
-        host=host, port=port
-    )
+    mongoengine.register_connection(**args)
 
 def db_conn_kill():
     mongoengine.disconnect_all()
